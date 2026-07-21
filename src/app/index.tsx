29c735458f5a88fabfc59ed18d0c971787d3c005
zeez09 <harunaabdulazeez766@gmail.com>
2026-07-21 18:14:32 +0100

    new updates ft homescreen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import Categories from "@/components/categories";
+import { StatusBar, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>PricePulse 🚀</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <StatusBar
+      translucent
+      backgroundColor="transparent"
+      barStyle="dark-content"
+      />
+
+      <View className="px-2 py-4">
+        <Categories />
+        
+
+
+      </View>
+      
+      
+    </SafeAreaView>
   );
 }
---
+++ src/components/categories.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Categories() {
+  return (
+    <View>
+      <Text className="text-red-500 text-3xl">Add Price</Text>
+    </View>
+  );
+}
