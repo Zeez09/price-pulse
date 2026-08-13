da470e89cbeba68aecb3a05d15d20f1422c27f41
zeez09 <harunaabdulazeez766@gmail.com>
2026-08-13 14:58:04 +0100

    new updates ft vector

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,5 +1,5 @@
-
-import { StyleSheet, View } from 'react-native';
+import { FONTS } from "@/constants/fonts";
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import BubbleIllustrations from "../../assets/images/illustrations/bubble-01.svg";
 import BubbleIllustrations2 from "../../assets/images/illustrations/bubble-02.svg";
@@ -20,6 +20,16 @@ export default function login() {
         <BubbleIllustrations3 width={100} height={100} style={styles.bubble3}/>
 
       </View>
+
+
+      <View style={styles.loginContainer}>
+        <Text style={styles.txt}>Login</Text>
+        <Text style={styles.txt1}>Good to see you back!</Text>
+
+        <TextInput placeholder="Email" style={styles.input}></TextInput>
+      </View>
+
+      <BubbleIllustrations width={200} height={200} style={styles.bubblebottom} pointerEvents="none"/>
     </SafeAreaView>
   )
 }
@@ -51,7 +61,47 @@ const styles = StyleSheet.create({
   bubble3: {
     position: "absolute",
     right: 0,
-    top: 150,
+    top: 250,
 
-  }
+  },
+
+  loginContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+    zIndex: 10,
+  },
+
+  txt: {
+    fontSize: 52,
+    fontFamily: FONTS.bold, 
+  },
+
+  txt1: {
+    fontSize: 19,
+    fontFamily: FONTS.regular,
+  },
+
+  input: {
+    width: "80%",
+    height: 70,
+    backgroundColor: "#D1D5DB",
+    borderRadius: 14,
+    paddingHorizontal: 12,
+    marginTop: 12,
+
+  },
+
+  bubblebottom: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    transform: [{ rotate: "180deg"}],
+    zIndex: 1,
+  },
+
+
+
+
 });
