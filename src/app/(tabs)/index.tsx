64add4af94b606f71b8a7485632c79ad685242ec
zeez09 <harunaabdulazeez766@gmail.com>
2026-09-21 18:01:40 +0100

    new updates ft home screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,7 +4,14 @@ import ProductCard from "@/components/productCard";
 import Swiper from "@/components/swiper";
 import { FONTS } from "@/constants/fonts";
 import { products } from "@/data/products";
-import { ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
@@ -17,37 +24,41 @@ export default function HomeScreen() {
       />
 
       <ScrollView style={styles.container}>
-        <HomeHeader/>
+        <HomeHeader />
         <Swiper />
         <Categories />
 
         <View style={styles.productSection}>
-        <View style={styles.productHeader}>
-        <Text style={styles.title}>Popular Products</Text>
-        <TouchableOpacity style={styles.seeAll}>
-          <Text style={styles.txt1}>View all</Text>
-        </TouchableOpacity>
-        </View>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          {products.map((product) => (
-            <ProductCard
-              key={product.id}
-              name={product.name}
-              size={product.size}
-              price={product.price}
-              image={product.image}
-            />
-          ))}
-        </ScrollView>
+          <View style={styles.productHeader}>
+            <Text style={styles.title}>Popular Products</Text>
+
+            <TouchableOpacity style={styles.seeAll}>
+              <Text style={styles.txt1}>View all</Text>
+            </TouchableOpacity>
+          </View>
+
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          >
+            {products.map((product) => (
+              <ProductCard
+                key={product.id}
+                name={product.name}
+                size={product.size}
+                price={product.price}
+                image={product.image}
+              />
+            ))}
+          </ScrollView>
         </View>
 
         <View style={styles.trendingSection}>
           <View style={styles.trendingHeader}>
-            <Text style={styles.title}> Trending Prices</Text>
+            <Text style={styles.title}>Trending Prices</Text>
+
             <TouchableOpacity style={styles.seeAll}>
-              <Text style={styles.txt1}>
-                See all
-              </Text>
+              <Text style={styles.txt1}>See all</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -89,13 +100,13 @@ const styles = StyleSheet.create({
     fontWeight: "700",
   },
 
-  txt1 : {
+  txt1: {
     fontFamily: FONTS.regular,
     fontSize: 14,
     fontWeight: "500",
   },
 
-    trendingSection: {
+  trendingSection: {
     gap: 16,
     marginTop: 20,
   },
@@ -105,6 +116,4 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
   },
-
-
 });
